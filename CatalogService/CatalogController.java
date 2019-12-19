2afce7aa58c9bceec7dd0c143003fd23d28e5927
sharmapoonam12 <58967451+sharmapoonam12@users.noreply.github.com>
2019-12-19 14:45:53 +0530

    Add files via upload

---
+++ CatalogService/CatalogController.java
@@ -0,0 +1,33 @@
+package com.publicissapient.catalog;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.publicissapient.catalog.dto.Product;
+import com.publicissapient.catalog.service.CatalogService;
+
+@RestController
+public class CatalogController {
+	
+	@Autowired
+	private CatalogService catalogService;
+	
+	
+
+	@RequestMapping("/catalog/product/{category}")
+	public List<Product> getInventoryProductByCategory(@PathVariable String category){	
+		return catalogService.getInventoryProductByCategory(category);		
+	}
+	
+	@RequestMapping("/catalog/product/{productId}")
+	public List<Product> getInventoryProductById(@PathVariable String productId){	
+		return catalogService.getInventoryProductById(productId);		
+	}
+	
+	
+
+}
