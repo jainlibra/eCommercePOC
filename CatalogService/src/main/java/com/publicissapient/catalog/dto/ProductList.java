767b0c991591fb25fd3452e6e7d9a28f78f5bba8
poosharm7 <poosharm7@WKWIN3400704.global.publicisgroupe.net>
2019-12-20 14:22:32 +0530

    changes committed

---
+++ CatalogService/src/main/java/com/publicissapient/catalog/dto/ProductList.java
@@ -0,0 +1,34 @@
+package com.publicissapient.catalog.dto;
+
+import java.util.List;
+
+public class ProductList  {
+	
+			
+			private List<Product> products;	
+
+			public ProductList() {
+				
+			}
+			
+			public ProductList(List<Product> products) {
+				this.products = products;
+				
+			}
+
+			public List<Product> getProducts() {
+				return products;
+			}
+
+			public void setProducts(List<Product> products) {
+				this.products = products;
+			}
+			
+			
+
+		} 
+		 
+
+
+
+
