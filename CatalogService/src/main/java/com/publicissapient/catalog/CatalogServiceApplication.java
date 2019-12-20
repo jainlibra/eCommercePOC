98f0cd7543245fa998bde679f8ef2a533e951ca2
poosharm7 <poosharm7@WKWIN3400704.global.publicisgroupe.net>
2019-12-20 15:13:49 +0530

    changes committed

--- CatalogService/src/main/java/com/publicissapient/catalog/CatalogServiceApplication.java
+++ CatalogService/src/main/java/com/publicissapient/catalog/CatalogServiceApplication.java
@@ -3,6 +3,7 @@ package com.publicissapient.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
@@ -15,6 +16,7 @@ public class CatalogServiceApplication {
 
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
